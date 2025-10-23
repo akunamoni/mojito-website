6f1c16ff52b7c7a322eac38520fea948389aeefe
vamshiinnings2 <vamshi.kunamoni@innings2.com>
2025-10-23 10:57:08 +0530

    Clean reinstall to fix crypto.hash issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 // vite.config.js
+import crypto from "node:crypto";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
+if (!crypto.hash) {
+  crypto.hash = (alg, data) =>
+    crypto.createHash(alg).update(data).digest("hex");
+}
 // ✅ Use export default instead of module.exports
 export default defineConfig({
   plugins: [tailwindcss()],
